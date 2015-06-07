f7ea2888afcf4c425d644886dbcb7e1aa15f6163
June <junenam89@hotmail.com>
2015-06-07 15:39:59 -0700

    with local storage

--- register.js
+++ register.js
@@ -1,33 +1,66 @@
 
 
-function registerFunction(email,firstName,lastName,userType,password) {
-
-
+function registerFunction(email,firstName,lastName,userType,password,rpassword) {
+ 
  Parse.initialize("q739A3XGOjqeLfXtl4vJXT33UENWvpesaAulyQPD", "uOPMZ9HUu9zDTsZfD7AFpACnmkBWJq5aruLsE3IC");
 
-  var hashpw = CryptoJS.SHA1(password);
+  var WorkflowUser = Parse.Object.extend("Users");
+  var query = new Parse.Query(WorkflowUser);
 
-  var TestObject = Parse.Object.extend("Users");
-  var testObject = new TestObject();
-  testObject.save(
-    {Email: email,
-    FirstName: firstName,
-    LastName: lastName,
-    UserType: userType,
-    Password: hashpw.toString(CryptoJS.enc.Base64)},
-      {
-        success: function(testObject) {
-          alert("successfully registered!")
-          window.location = "home.html";
-        },
-        error: function(testObject, error) {
-          alert("Signup Failed. :( " + error.message);
-          // The save failed.
-          // error is a Parse.Error with an error code and message.
+  query.equalTo("Email", email);
+
+   query.find({
+  success: function(results) {
+
+    if(results != ""){
+      alert("This userID already exists. Please try again.");
+    
+    }
+
+    else{
+        
+        if(password != rpassword){
+        alert('You enetered 2 different passwords. Please Confirm your passwords.');
+        event.preventDefault();
         }
-  });
 
-  event.preventDefault();
+
+        else{
+            
+
+              var hashpw = CryptoJS.SHA1(password);
+
+              var TestObject = Parse.Object.extend("Users");
+              var testObject = new TestObject();
+
+        
+             testObject.save(
+                {Email: email,
+                FirstName: firstName,
+                LastName: lastName,
+                UserType: userType,
+                Password: hashpw.toString(CryptoJS.enc.Base64)},
+                  {
+                    success: function(testObject) {
+                      alert("successfully registered!");
+                      window.location = "home.html";
+                    },
+                    error: function(testObject, error) {
+                      alert("Signup Failed. :( " + error.message);
+                      // The save failed.
+                      // error is a Parse.Error with an error code and message.
+                    }
+              });
+        }
+    }
+  },
+  error: function(error) {
+    alert("qurey Error: " + error.code + " " + error.message);
+  }
+});
+
+event.preventDefault();
+ 
 }
 
 
@@ -45,7 +78,7 @@ function login(email, password){
   success: function(results) {
 
     if(results == ""){
-      alert("Coudln't find your e-mail. Please try again.");
+      alert("Couldn't find your e-mail. Please try again.");
     }
       
       var object = results[0];
@@ -54,6 +87,9 @@ function login(email, password){
 	  var hashedpw = CryptoJS.SHA1(password);
 
       if(pw == hashedpw.toString(CryptoJS.enc.Base64)){    
+
+        //localStorage.setItem('userID', JSON.stringify(email));
+        localStorage.setItem('userID',email);
         window.location = 'main.html'
       }
       else{
